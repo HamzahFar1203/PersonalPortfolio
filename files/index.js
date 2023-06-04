ae2fa7e8986ea401d495457c75deb9fae3a0f021
Hamzah Farooqui <72969002+HamzahFar1203@users.noreply.github.com>
2023-06-04 17:12:13 -0500

    Complete Change
    Completely changed the website, added CSS Flexbox.

--- files/index.js
+++ files/index.js
@@ -1,10 +1,11 @@
-window.onload = function () {
-    var list_of_langs = document.querySelectorAll("#languages_list li").length;
-    for (var i = 0; i < list_of_langs; i++) {
-        document.querySelectorAll("#languages_list li")[i].setAttribute("class", "known_languages");
-    }
-}
+$("body").hide().fadeIn(1000);
 
-$(function() {
-    $("#profile_pic_div, #profile_content p, #center_content").fadeIn(1000);
-});
+/*
+    SOON TO REMOVE/CHANGE
+*/
+// window.onload = function () {
+//     var list_of_langs = document.querySelectorAll("#languages_list li").length;
+//     for (var i = 0; i < list_of_langs; i++) {
+//         document.querySelectorAll("#languages_list li")[i].setAttribute("class", "known_languages");
+//     }
+// }
